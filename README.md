f7c36defdf75ad3dfadd9b41b13943ae2bb19b04
JakobBull <jakob.bull@yahoo.de>
2023-06-10 16:47:21 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+## This is the repo for a financial econometrics project, creating portfolios that maximise Sharpe ratio
+
+## About
+
+Written by Jakob Bull, Patrick Dickinson, and Zachary Parsons for info contact <jakob.bull@yahoo.de>
+
+## Setup
+
+To quickly get setup follow these steps in a Terminal:
+
+`conda create --name stocks`
+
+Then activate the environment
+
+`conda activate stocks`
+
+Install the requirements:
+
+`conda install --file requirements.txt`
---
+++ strategies/nfixedbaskets.py
@@ -0,0 +1,14 @@
+
+
+class StrategyNFixedBaskets:
+
+    def __init__(self, args=None) -> None:
+        self.args = args
+
+    def execute(self, source_data, train_dates, test_dates):
+        positions = {}
+        source_data = source_data[source_data['date'].isin(test_dates)]
+        for date in test_dates:
+            available_stocks = source_data.loc[source_data['date'] == date, 'order_book_id'].tolist()
+            positions[date] = {stock: 1/len(available_stocks) for stock in available_stocks}
+        return positions
