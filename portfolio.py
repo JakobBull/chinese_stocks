7a9a48f83c99f5cad928785f4ba5ccaf79ca9714
JakobBull <jakob.bull@yahoo.de>
2023-06-10 16:32:15 +0800

    Portfolio class

---
+++ portfolio.py
@@ -0,0 +1,91 @@
+import numpy as np
+import pandas as pd
+
+
+"""
+Portfolio to generate portfolio objects.
+
+Usage:
+Instantiate object and then use the run() method, which takes as arguments a strategy object and the data. Optional args also supported
+Refer to example for usage. Models are evaluated automatically (successful evaluation confirmed with output 'Activated.')
+You can use the self attribute to access a dataframe that includes the portfolio at a given time and the stats attribute for a quick performance overview.
+
+"""
+class Portfolio:
+
+    def  __init__(self):
+        self.dates = []
+        self.positions = {}
+        self.valid_stocks = {}
+        self.stats = {}
+        self.data = pd.DataFrame(columns=['date','positions', 'n_positions', 'return'])
+
+    def set_valid_stocks(self, valid_stocks):
+        self.valid_stocks = valid_stocks
+
+    def change_position(self, date, position, error=10e-6):
+        if date not in self.dates:
+            self.dates.append(date)
+        if abs(sum(position.values()) - 1) < error:
+            if all(key in self.valid_stocks[date] for key in position.keys()):
+                self.positions[date] = position
+            else:
+                for key in position.keys():
+                    if key not in self.valid_stocks[date]:
+                        if position[key] > 0:
+                            print(f'Stock {key} not defined at date {date}.')
+        else:
+            raise print("The position weighting does not add to 1.")
+
+    def well_defined(self):
+        nones = not all(self.positions.values())
+
+        if nones:
+            for key, value in self.positions.items():
+                if not value:
+                    print(f"Please define a portfolio position for {key}")
+        else:
+            print("Portfolio well defined.")
+            self.is_well_defined = True
+
+    def activate(self):
+        if not self.is_well_defined:
+            print("Please make sure the portfolio is well defined first by calling Portfolio.well_defined().")
+        else:
+        
+            #based on the positions at any given date, reweigh portfolio and calculate the portfolio return, write new row with return and key info to data DataFrame
+            for date in self.dates:
+                month_positions = self.positions[date]
+                month_returns = {position: self.source_data.groupby(['order_book_id', 'date'])['exret'].get_group((position, date)) for position in month_positions.keys()}
+                total_return = np.array([month_positions[key] * month_returns[key] for key in month_positions.keys()]).sum()
+                row_data = [date, self.positions[date], len(self.positions[date]), total_return]
+                self.data = self.data.append(pd.Series(row_data, index=self.data.columns), ignore_index=True)
+
+            #Calculate various portfolio statistics such as standard deviations and Sharpe ratio
+            self.data['std'] = self.data['return'].rolling(12).std()
+            self.data['Sharpe'] = self.data['return'].div(self.data['std'])
+            self.data = self.data.iloc[12:, :]
+            self.stats["Portfolio Return"] = np.prod(self.data['return']+1)
+            self.stats["Portfolio std"] = self.data['return'].std()
+            self.stats["Portfolio Sharpe"] = self.stats["Portfolio Return"]/self.stats["Portfolio std"]
+            print("Activated.")
+
+    def implement_strategy(self, positions):
+        for date, position in positions.items():
+            self.change_position(date, position)
+
+    def train_test_split(self, train_test_split=0.7):
+        dates = sorted(list(set(self.source_data.date)))
+        self.train_dates = dates[:int(len(dates)*train_test_split)]
+        #Note the overlap,this is only used to calculate std rolling average
+        self.test_dates = dates[int(len(dates)*train_test_split)-12:]
+
+    def run(self, strategy_method, source_data, args=None):
+        self.source_data = source_data
+        self.set_valid_stocks({date: list(self.source_data.groupby("date")["order_book_id"].get_group(date)) for date in set(self.source_data.date)})
+        self.train_test_split()
+        strategy = strategy_method(args)
+        self.implement_strategy(strategy.execute(source_data, self.train_dates, self.test_dates))
+        print(self.train_dates[-1])
+        self.well_defined()
+        self.activate()
